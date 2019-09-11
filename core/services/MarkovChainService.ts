aefeccc69dbe61f0530f15bb9c1fa0587696dbcd
Robbie Hickey <Robbie.Hickey@alliedelec.com>
2019-09-11 15:12:06 -0500

    add randomization to markov chain responses

--- core/services/MarkovChainService.ts
+++ core/services/MarkovChainService.ts
@@ -8,9 +8,14 @@ markovChain.seed(markovData.join("\n"), function() {
   console.log("Markov chain seeded successfully");
 });
 
+function getRandomNumber(upToInteger) {
+  // minumum 3 word sentence
+  return Math.floor(Math.random() * upToInteger) + 3;
+}
+
 export function getRandomSentence(slackText): ChatPostMessageArguments {
   return {
-    text: markovChain.respond(slackText, 5).join(" "),
+    text: markovChain.respond(slackText, getRandomNumber(15)).join(" "),
     channel: ""
   };
 }
